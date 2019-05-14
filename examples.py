ac1c2fa364986a1acad8eea54d50b038502cdc26
sharonsyra <sharonwaithii@gmail.com>
2019-05-14 22:41:46 +0300

    Add examples file to show sample usage

---
+++ examples.py
@@ -0,0 +1,14 @@
+from wrapper import MySQLWrapper
+from collections import namedtuple
+
+Table = namedtuple('Table', 'table_name datatype')
+bob = Table(table_name='name', datatype='VARCHAR(23)')
+jane = Table(table_name='age', datatype='VARCHAR(35)')
+
+wrapper = MySQLWrapper.start()
+wrapper.create_table('user', [bob, jane])
+wrapper.insert_values('user', ['age','name'], ['90', '67'])
+wrapper.fetch_all('user')
+wrapper.fetch_limit('user', 1)
+wrapper.update_table('user', 'age', '48', 'age', '45')
+wrapper.delete_table('fir1')
