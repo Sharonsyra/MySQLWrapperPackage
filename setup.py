af54c5fb600791474abd560967bb3ff7f0745ead
sharonsyra <sharonwaithii@gmail.com>
2019-05-15 09:05:21 +0300

    Add dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='mysqlwrapperpackage',
-      version='0.1',
+      version='0.2',
       description='MySQL Python Wrapper Package',
+      long_description=long_description,
       url='https://github.com/Sharonsyra/MySQLWrapperPackage',
       author='Sharon Waithira',
       author_email='sharonwaithii@gmail.com',
       license='MIT',
+      install_requires=[
+          'pymsql',
+          'injector',
+      ],
       packages=['MySQLWrapperPackage'],
       zip_safe=False)
